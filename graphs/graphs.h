716ef9dd7b92a69a65aaf467672297757d03e371
Crisstaal <muyungacrys@gmail.com>
2025-03-30 13:16:23 -0500

    save

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -4,42 +4,65 @@
 #include <stddef.h>
 
 /**
- * struct vertex_s - Structure for a graph vertex (node)
- * @index: Index of the vertex in the adjacency list (unique)
- * @content: Name or data of the vertex (e.g., city name)
- * @nb_edges: Number of edges connected to the vertex
- * @edges: Pointer to the first edge (adjacency list)
- * @next: Pointer to the next vertex in the adjacency list
+ * enum edge_type_e - Enumerates the different types of
+ * connection between two vertices
+ *
+ * @UNIDIRECTIONAL: The connection is made only in one way
+ * @BIDIRECTIONAL: The connection is made in two ways
  */
-typedef struct vertex_s
+typedef enum edge_type_e
 {
-	size_t index;
-	char *content;
-	size_t nb_edges;
-	struct edge_s *edges;
-	struct vertex_s *next;
-} vertex_t;
+	UNIDIRECTIONAL = 0,
+	BIDIRECTIONAL
+} edge_type_t;
+
+/* Define the structure temporarily for usage in the edge_t */
+typedef struct vertex_s vertex_t;
 
 /**
- * struct edge_s - Structure for a graph edge (connection)
- * @dest: Pointer to the destination vertex
- * @next: Pointer to the next edge in the adjacency list
+ * struct edge_s - Node in the linked list of edges for a given vertex
+ * A single vertex can have many edges
+ *
+ * @dest: Pointer to the connected vertex
+ * @next: Pointer to the next edge
  */
 typedef struct edge_s
 {
-	struct vertex_s *dest;
+	vertex_t *dest;
 	struct edge_s *next;
 } edge_t;
 
 /**
- * struct graph_s - Graph structure
- * @nb_vertices: Number of vertices in the graph
- * @vertices: Pointer to the head of the adjacency list
+ * struct vertex_s - Node in the linked list of vertices in the adjacency list
+ *
+ * @index: Index of the vertex in the adjacency list.
+ * @content: Custom data stored in the vertex (here, a string)
+ * @nb_edges: Number of conenctions with other vertices in the graph
+ * @edges: Pointer to the head node of the linked list of edges
+ * @next: Pointer to the next vertex in the adjacency linked list
+ *   This pointer points to another vertex in the graph, but it
+ *   doesn't stand for an edge between the two vertices
+ */
+struct vertex_s
+{
+	size_t index;
+	char *content;
+	size_t nb_edges;
+	edge_t *edges;
+	struct vertex_s *next;
+};
+
+/**
+ * struct graph_s - Representation of a graph
+ * We use an adjacency linked list to represent our graph
+ *
+ * @nb_vertices: Number of vertices in our graph
+ * @vertices: Pointer to the head node of our adjacency linked list
  */
 typedef struct graph_s
 {
 	size_t nb_vertices;
-	struct vertex_s *vertices;
+	vertex_t *vertices;
 } graph_t;
 
 /* Graph functions */
