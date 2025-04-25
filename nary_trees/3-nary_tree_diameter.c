b47feedad94c16fedc58c69300d378994ba0e1e3
Crisstaal <muyungacrys@gmail.com>
2025-04-25 09:23:05 -0500

    try

--- nary_trees/3-nary_tree_diameter.c
+++ nary_trees/3-nary_tree_diameter.c
@@ -1,11 +1,11 @@
 #include "nary_trees.h"
+#include <stdlib.h>
 
 /**
- * max - Returns the greater of two size_t values
- * @a: First value
- * @b: Second value
- *
- * Return: The maximum of a and b
+ * max - Returns the maximum of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the larger number
  */
 static size_t max(size_t a, size_t b)
 {
@@ -13,16 +13,16 @@ static size_t max(size_t a, size_t b)
 }
 
 /**
- * depth_and_diameter - Recursively finds max depth and updates diameter
- * @node: Pointer to current node
- * @diameter: Pointer to current diameter
- *
- * Return: Max depth from this node downward
+ * dfs - Helper to compute height and update diameter
+ * @node: current node
+ * @diameter: pointer to store max diameter
+ * Return: height of the node
  */
-static size_t depth_and_diameter(const nary_tree_t *node, size_t *diameter)
+static size_t dfs(nary_tree_t const *node, size_t *diameter)
 {
-	size_t max1 = 0, max2 = 0, d, depth = 0;
-	nary_tree_t *child;
+	size_t first_max = 0, second_max = 0, height = 0;
+	nary_tree_t *child = NULL;
+	size_t h;
 
 	if (!node)
 		return (0);
@@ -30,32 +30,28 @@ static size_t depth_and_diameter(const nary_tree_t *node, size_t *diameter)
 	child = node->children;
 	while (child)
 	{
-		d = depth_and_diameter(child, diameter);
-		if (d > max1)
+		h = dfs(child, diameter);
+		if (h > first_max)
 		{
-			max2 = max1;
-			max1 = d;
+			second_max = first_max;
+			first_max = h;
 		}
-		else if (d > max2)
+		else if (h > second_max)
 		{
-			max2 = d;
+			second_max = h;
 		}
 		child = child->next;
 	}
 
-	/* Update diameter if this path is longer */
-	if (diameter && (max1 + max2) > *diameter)
-		*diameter = max1 + max2;
-
-	/* Return depth from this node */
-	return (max1 + 1);
+	*diameter = max(*diameter, first_max + second_max);
+	height = 1 + first_max;
+	return (height);
 }
 
 /**
  * nary_tree_diameter - Computes the diameter of an N-ary tree
- * @root: Pointer to the root node
- *
- * Return: Diameter of the tree
+ * @root: pointer to the root node
+ * Return: the diameter
  */
 size_t nary_tree_diameter(nary_tree_t const *root)
 {
@@ -64,6 +60,6 @@ size_t nary_tree_diameter(nary_tree_t const *root)
 	if (!root)
 		return (0);
 
-	depth_and_diameter(root, &diameter);
+	dfs(root, &diameter);
 	return (diameter);
 }
